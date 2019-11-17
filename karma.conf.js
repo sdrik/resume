a892aa49dcda523913d50ef2d2f348ca99f9f2c3
Cédric Schieli <cschieli@gmail.com>
2019-11-17 14:16:00 +0100

    Enable headless tests

--- karma.conf.js
+++ karma.conf.js
@@ -26,6 +26,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: {
+      ChromiumHeadlessNoSandbox: {
+        base: 'ChromiumHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
